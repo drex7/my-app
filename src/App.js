df35f54bded8819755c7d150111dc5a6928a22d1
Derrick Asamoah <asamoahderrick7@gmail.com>
2021-11-28 23:55:04 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+		const package = 'com.android.settings';
   return (
     <div className="App">
       <header className="App-header">
-        <a href="intent://scan/#Intent;scheme=zxing;package=com.google.zxing.client.android;end">Open Settings</a>
+        <a href="intent://scan/#Intent;package=com.android.settings;end">Open Settings</a>
+		  <p><a href={`intent:#Intent;action=BLUETOOTH_SETTINGS;package=${package};end`}>Bluetooth Settings</a>
+		  </p>
+		  <p><a href={`intent:#Intent;action=${package}.DISPLAY_SETTINGS;package=${package};end`}>DISPLAY SETTINGS</a>
+		  </p>
 		<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
