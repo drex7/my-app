7bed425c9e3e903b1e3e7b463342ac8718923cee
Derrick Asamoah <asamoahderrick7@gmail.com>
2021-11-28 23:59:44 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-		const package = 'com.android.settings';
+		const pkg = 'com.android.settings';
   return (
     <div className="App">
       <header className="App-header">
         <a href="intent://scan/#Intent;package=com.android.settings;end">Open Settings</a>
-		  <p><a href={`intent:#Intent;action=BLUETOOTH_SETTINGS;package=${package};end`}>Bluetooth Settings</a>
+		  <p><a href={`intent:#Intent;action=BLUETOOTH_SETTINGS;package=${pkg};end`}>Bluetooth Settings</a>
 		  </p>
-		  <p><a href={`intent:#Intent;action=${package}.DISPLAY_SETTINGS;package=${package};end`}>DISPLAY SETTINGS</a>
+		  <p><a href={`intent:#Intent;action=${pkg}.DISPLAY_SETTINGS;package=${pkg};end`}>DISPLAY SETTINGS</a>
 		  </p>
 		<p>
           Edit <code>src/App.js</code> and save to reload.
